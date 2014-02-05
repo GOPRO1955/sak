88b8156f4d96271df8a9e334a5ed5b429895f96f
Jeppe Pihl <jpihl08@gmail.com>
2014-02-05 15:46:46 +0100

    less gtest

--- test/sak_tests.cpp
+++ test/sak_tests.cpp
@@ -3,14 +3,11 @@
 #include <cstdint>
 #include <vector>
 
-GTEST_API_ int main(int argc, char** argv)
+int main()
 {
-    testing::InitGoogleTest(&argc, argv);
-
     std::vector<uint8_t> m_vector;
     m_vector.resize(10);
 
     std::vector<uint8_t> data(32);
     EXPECT_EQ(32U, data.size());
-    return RUN_ALL_TESTS();
 }
