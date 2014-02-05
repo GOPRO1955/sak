0c49c8c944bab69fc0961a0c6e0e106eabbdff78
Jeppe Pihl <jpihl08@gmail.com>
2014-02-05 15:37:14 +0100

    no buffer

--- test/sak_tests.cpp
+++ test/sak_tests.cpp
@@ -34,38 +34,11 @@
 #include <vector>
 #include <algorithm>
 
-namespace sak
-{
-    namespace {
-        /// Helper class that implements a simple buffer. Data can be written
-        /// into the buffer which appends it to the existing data. Keeps
-        /// track of the amount of data in the buffer.
-        class buffer
-        {
-        public:
-
-            /// Constructs a new zero sized buffer
-            buffer()
-            { }
-
-            /// Constructs a new buffer with an initial capacity
-            buffer(uint32_t capacity)
-            {
-                m_vector.resize(capacity);
-            }
-
-        private:
-
-            /// Internal storage
-            std::vector<uint8_t> m_vector;
-        };
-    }
-}
-
 TEST(TestBuffer, append_to_initialized)
 {
     {
-        sak::buffer b(10);
+        std::vector<uint8_t> m_vector;
+        m_vector.resize(10);
         EXPECT_EQ(0U, 0U);
 
         std::vector<uint8_t> data(32, 'x');
