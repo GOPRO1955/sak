4888ba4b9128abf1fd580d9a0fe13277513ea86f
Jeppe Pihl <jpihl08@gmail.com>
2014-02-05 15:57:21 +0100

    no stdint

--- test/sak_tests.cpp
+++ test/sak_tests.cpp
@@ -1,11 +1,10 @@
-#include <cstdint>
 #include <vector>
 #include <iostream>
 
 int main()
 {
-    std::vector<uint8_t> v1(10);
+    std::vector<char> v1(10);
     std::cout << v1.size() << std::endl;
-    std::vector<uint8_t> v2(32);
+    std::vector<char> v2(32);
     std::cout << v2.size() << std::endl;
 }
