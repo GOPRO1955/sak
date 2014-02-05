a181bcaa282f195e1362e4c41ba9c1105807cae0
Jeppe Pihl <jpihl08@gmail.com>
2014-02-05 15:42:18 +0100

    no tests

--- test/sak_tests.cpp
+++ test/sak_tests.cpp
@@ -3,20 +3,15 @@
 #include <cstdint>
 #include <vector>
 
-TEST(TestBuffer, append_to_initialized)
-{
-    {
-        std::vector<uint8_t> m_vector;
-        m_vector.resize(10);
-
-        std::vector<uint8_t> data(32);
-        EXPECT_EQ(32U, data.size());
-    }
-}
-
 GTEST_API_ int main(int argc, char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
 
+    std::vector<uint8_t> m_vector;
+    m_vector.resize(10);
+
+    std::vector<uint8_t> data(32);
+    EXPECT_EQ(32U, data.size());
+    EXPECT_EQ(1,2);
     return RUN_ALL_TESTS();
 }
