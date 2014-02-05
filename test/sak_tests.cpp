26792870079d57ab46d182dfdf567f34a3cbb808
Jeppe Pihl <jpihl08@gmail.com>
2014-02-05 15:55:20 +0100

    simple simple

--- test/sak_tests.cpp
+++ test/sak_tests.cpp
@@ -1,13 +1,10 @@
 #include <cstdint>
 #include <vector>
+#include <iostream>
 
 int main()
 {
-    std::vector<uint8_t> m_vector(10);
-
-    std::vector<uint8_t> data(32);
-    if (data.size() == 32)
-        return 0;
-    else
-        return 1;
+    std::vector<uint8_t> v1(10);
+    std::vector<uint8_t> v1(32);
+    std::cout << data.size() << std::endl;
 }
